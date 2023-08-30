43fd6a2c12779e1bfa7bc3c63a68691e579a8ae7
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2023-08-30 09:49:32 +0200

    Add platform project for minimal linux example

--- linux/MinimalLinux/application/MinimalLinux/platform.tcl
+++ linux/MinimalLinux/application/MinimalLinux/platform.tcl
@@ -1,31 +1,24 @@
 # 
 # Usage: To re-create this platform project launch xsct with below options.
-# xsct G:\Nextcloud_\Git\ZYBO\linux\MinimalLinux\application\MinimalLinux\platform.tcl
+# xsct C:\Users\konta\Desktop\a\MinimalLinux\platform.tcl
 # 
 # OR launch xsct and run below command.
-# source G:\Nextcloud_\Git\ZYBO\linux\MinimalLinux\application\MinimalLinux\platform.tcl
+# source C:\Users\konta\Desktop\a\MinimalLinux\platform.tcl
 # 
 # To create the platform in a different location, modify the -out option of "platform create" command.
 # -out option specifies the output directory of the platform project.
 
 platform create -name {MinimalLinux}\
--hw {G:\Nextcloud_\Git\ZYBO\linux\MinimalLinux\hardware\System_wrapper.xsa}\
--proc {ps7_cortexa9} -os {linux} -no-boot-bsp -fsbl-target {psu_cortexa53_0} -out {G:/Nextcloud_/Git/ZYBO/linux/MinimalLinux/application}
+-hw {\\wsl.localhost\Ubuntu\home\daniel\Projects\ZYBO\linux\MinimalLinux\hardware\System_wrapper.xsa}\
+-proc {ps7_cortexa9} -os {linux} -no-boot-bsp -out {C:/Users/konta/Desktop/a}
 
 platform write
-domain config -rootfs {G:\Nextcloud_\Git\ZYBO\rootfs\rootfs.cpio.gz}
+platform active {MinimalLinux}
+platform active {MinimalLinux}
+domain config -bootmode {qspi}
 platform write
-domain config -sysroot {G:\Nextcloud_\Git\ZYBO\linux\MinimalLinux\sdk\sysroots\cortexa9t2hf-neon-xilinx-linux-gnueabi}
+domain config -bootmode {sd}
+platform write
+domain config -boot {/home/daniel/Projects/ZYBO/linux/MinimalLinux/petalinux/images/linux}
 platform write
 platform generate
-platform active {MinimalLinux}
-domain config -rootfs {G:\Nextcloud_\Git\ZYBO\linux\MinimalLinux\rootfs\rootfs.cpio.gz}
-platform write
-platform generate -domains 
-platform clean
-platform generate
-platform active {MinimalLinux}
-domain config -rootfs {G:\Nextcloud\Git\ZYBO\linux\MinimalLinux\rootfs\rootfs.cpio.gz}
-platform write
-domain config -sysroot {G:\Nextcloud\Git\ZYBO\linux\MinimalLinux\sdk\sysroots\cortexa9t2hf-neon-xilinx-linux-gnueabi}
-domain config -sysroot {G:\Nextcloud\Git\ZYBO\linux\MinimalLinux\sdk\sysroots\cortexa9t2hf-neon-xilinx-linux-gnueabi}
